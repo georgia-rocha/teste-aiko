bd1ddfc0b583d7585fe63cf2eb08d0d08478940f
Daniel Batista <danielbped@gmail.com>
2025-04-04 18:41:12 -0300

    feat: initial structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './assets/img/aiko.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+         <img src={logo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>Hello Word testee</h1>
+    </>
+  )
+}
+
+export default App

8330eb2161b0a63b8ae8a2363face7a1c9abd244
Georgia Rocha <georgiab543@gmail.com>
2025-04-06 13:41:46 -0300

    feat: add NotFount page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,27 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const NotFound = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.error(
+      "404 Error: User attempted to access non-existent route:",
+      location.pathname
+    );
+  }, [location.pathname]);
+
+  return (
+    <div>
+      <div>
+        <h1>404</h1>
+        <p>Oops! Page not found</p>
+        <a href="/">
+          Return to Home
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
